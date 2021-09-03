7489d27082904525bceb4a38aaefc08678ff8bc2
enzo-pagliacci-fiap <RM87203@fiap.com.br>
2021-09-03 17:05:58 -0300

    arrumando a estilização

--- homereact/src/App.js
+++ homereact/src/App.js
@@ -1,50 +1,56 @@
 import React from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { faBook, faExclamationTriangle, faMugHot } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
 
 
 export default function App() {
 
 return(
+  
 <>
 <div id="container">
     <div id="main">
       {/* <!-- inicio menu  --> */}
-      <nav class="navbar navbar-expand-lg navbar-light back-menu">
-        <div class=" container-fluid">
+      <nav className="navbar navbar-expand-lg navbar-light back-menu">
+        <div className=" container-fluid">
           <div>
-            <a href="./home.html" class="m-0"> <img src="./img/logo.png" alt="cerebro meditando, nome: calmamente"
-                class="logo m-0"></img> </a>
+            <a  href="./home.html" className="m-0 linkreact"> <img src="./img/logo.png" alt="cerebro meditando, nome: calmamente"
+                className="logo m-0"></img> </a>
           </div>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
             aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"></button>
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           
-          <div class="justify-content-end collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
+          <div className="justify-content-end collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
               <hr></hr>
-              <a class="menu-di  mx-1" aria-current="page" href="./estouEmCriseCadastro.html">Estou em crise</a>
-              <a class="menu-di mx-1" href="./queroMeAcalmar.html">Quero me acalmar</a>
-              <a class="menu-di mx-1" href="./informacoes.html">Informações</a>
-              <div class="perfi-usu">
-                <button class="menu-dir mx-1" id="bt-perfi" onclick='mostra_ocultaper()' href="#"><img
-                    src="./img/icon-usuario.png" class="men-home" alt=""></img></button>
+              <a className="menu-di  mx-1 linkreact" aria-current="page" href="./estouEmCriseCadastro.html">Estou em crise</a>
+              <a className="menu-di mx-1 linkreact" href="./queroMeAcalmar.html">Quero me acalmar</a>
+              <a className="menu-di mx-1 linkreact" href="./informacoes.html">Informações</a>
+              <div className="perfi-usu">
+                <button className="menu-dir mx-1" id="bt-perfi" onclick='mostra_ocultaper()' href="#"><img
+                    src="./img/icon-usuario.png" className="men-home" alt=""></img></button>
                 {/* <!-- inicio do perfil --> */}
-                <div class="per-text" id="bt-quaper">
-                  <a href="#"><i class="fas fa-user-edit i-per-fil"></i></a>
-                  <i class="fas fa-question-circle i-imusu"></i>
-                  <h4 class="p-nomus ">Xandao de Combate</h4>
-                  <hr class="hr-per" size="4"></hr>
-                  <h5 class="nomus-p">Missilbalistico@intercontinental</h5>
-                  <hr class="hr-per" size="4"></hr>
-                  <a href="#">
-                    <div class=" per-falabo">
-                      <h6 class="pa-per">Falar com o bot</h6>
-                      <i class="fas fa-robot ic-bot-per ic-fabo"></i>
+                <div className="per-text" id="bt-quaper">
+                  <a href="#"><i className="fas fa-user-edit i-per-fil linkreact"></i></a>
+                  <i className="fas fa-question-circle i-imusu"></i>
+                  <h4 className="p-nomus ">Xandao de Combate</h4>
+                  <hr className="hr-per" size="4"></hr>
+                  <h5 className="nomus-p">Missilbalistico@intercontinental</h5>
+                  <hr className="hr-per" size="4"></hr>
+                  <a href="#" className="linkreact">
+                    <div className=" per-falabo">
+                      <h6 className="pa-per">Falar com o bot</h6>
+                      <i className="fas fa-robot ic-bot-per ic-fabo"></i>
                     </div>
                   </a>
-                  <div class="mrgt-media mrgr-pequena d-flex justify-content-end">
-                    <button type="button" class="bt-sape" id="idSaiPer" onclick='saiper()'>Sair <i
-                        class="fas fa-sign-in-alt ic-sape"></i></button>
+                  <div className="mrgt-media mrgr-pequena d-flex justify-content-end">
+                    <button type="button" className="bt-sape" id="idSaiPer" onclick='saiper()'>Sair <i
+                        className="fas fa-sign-in-alt ic-sape"></i></button>
                   </div>
                 </div>
                 {/* <!-- fim do perfil --> */}
@@ -57,21 +63,21 @@ return(
 
       {/* <!-- inicio dos cards de 3 funcionalidade --> */}
       {/* <!-- card 1 --> */}
-      <div class="mrgx-pequena mrgt-pequena">
-        <div class="row m-0 row-cols-1 row-cols-md-3 g-4">
-          <div class="col">
-            <div class="ho-bor">
-              <a href="estouEmCriseCadastro.html">
+      <div className="mrgx-pequena mrgt-pequena">
+        <div className="row m-0 row-cols-1 row-cols-md-3 g-4">
+          <div className="col">
+            <div className="ho-bor">
+              <a className="linkreact" href="estouEmCriseCadastro.html">
                  
-                <div class="card h-100"> {/*style="border: 0px; position: static;"*/}
+                <div className="card h-100" style={{border: "0px", position: "static"}}> {/**/}
 
                   {/* <!-- <img src="..." class="card-img-top" alt="..."> --> */}
-                  <div class="d-flex justify-content-center mrgt-media">
-                    <i class="fas fa-exclamation-triangle tam-funcprin" ></i> {/*style="color: crimson;"*/}
+                  <div className="d-flex justify-content-center mrgt-media">
+                    <FontAwesomeIcon icon={faExclamationTriangle} className="tam-funcprin" style={{color: "crimson"}} />
                   </div>
-                  <div class="card-body esp-car">
-                    <h5 class="card-title d-flex justify-content-center titulos">Estou em crise</h5>
-                    <p class="card-text textos " >{/*style="text-align: center; "*/}Se estiver em uma crise {/*style="text-align: center; "*/}
+                  <div className="card-body esp-car">
+                    <h5 className="card-title d-flex justify-content-center titulos">Estou em crise</h5>
+                    <p className="card-text textos " style={{textAlign: 'center'}}>Se estiver em uma crise 
                       de ansiedade entre
                       aqui
                       para fazer alguns exercícios recomendados pra você, ou personalize seus exercícios.</p>
@@ -82,20 +88,20 @@ return(
           </div>
 
           {/* <!-- card 2 --> */}
-          <div class="col ">
-            <div class="ho-bor">
-              <a href="queroMeAcalmar.html">
+          <div className="col ">
+            <div className="ho-bor">
+              <a className="linkreact" href="queroMeAcalmar.html">
 
-                <div class="card h-100" > {/*style="border: 0px; position: static;"*/}
+                <div className="card h-100" style={{border: "0px", position: "static"}}> 
 
                   {/* <!-- <img src="..." class="card-img-top" alt="..."> --> */}
-                  <div class="d-flex justify-content-center mrgt-media">
-                    <i class="fas fa-mug-hot tam-funcprin" ></i> {/*style="color: rgb(204, 204, 25)"*/}
+                  <div className="d-flex justify-content-center mrgt-media">
+                    <FontAwesomeIcon icon={faMugHot} className="tam-funcprin" style={{color: "rgb(204, 204, 25)"}} />
                   </div>
-                  <div class="card-body esp-card">
-                    <h5 class="card-title d-flex justify-content-center titulos" >Quero me {/*style="text-align: center; "*/}
+                  <div className="card-body esp-card">
+                    <h5 className="card-title d-flex justify-content-center titulos" style={{textAlign: 'center'}}>Quero me 
                       acalmar</h5>
-                    <p class="card-text textos " >Quando estiver {/*style="text-align: center; "*/}
+                    <p className="card-text textos " style={{textAlign: 'center'}}>Quando estiver 
                       começando
                       a ficar ansioso e
                       precisar se acalmar, entre nessa área para exercícios mais específicos pra esse momento, ou
@@ -108,19 +114,20 @@ return(
           </div>
 
           {/* <!-- card 3 --> */}
-          <div class="col">
-            <div class="ho-bor">
-              <a href="informacoes.html">
+          <div className="col">
+            <div className="ho-bor">
+              <a className="linkreact" href="informacoes.html">
 
-                <div class="card h-100 " > {/*style="border: 0px; position: static;"*/}
+                <div className="card h-100 " style={{border: '0px', position: 'static'}} >
 
                   {/* <!-- <img src="..." class="card-img-top" alt="..."> --> */}
-                  <div class="d-flex justify-content-center mrgt-media">
-                    <i class="fas fa-book tam-funcprin" ></i> {/*style="color: rgb(69, 165, 202);"*/}
+                  <div className="d-flex justify-content-center mrgt-media">
+                    <FontAwesomeIcon icon={faBook} className="tam-funcprin" style={{color: "rgb(69, 165, 202)"}} />
+                    
                   </div>
-                  <div class="card-body">
-                    <h5 class="card-title d-flex justify-content-center titulos">Informações</h5>
-                    <p class="card-text textos" > A chave para diminuir sua {/*style="text-align: center;"*/}
+                  <div className="card-body">
+                    <h5 className="card-title d-flex justify-content-center titulos">Informações</h5>
+                    <p className="card-text textos" style={{textAlign: 'center'}} > A chave para diminuir sua 
                       ansiedade é o
                       autoconhecimento e entender como a ansiedade funciona, portanto nessa área você poderá encontrar
                       e-books, pesquisas e vídeos informacionais.</p>
@@ -132,8 +139,8 @@ return(
         </div>
       </div>
       {/* <!-- fim dos cards de 3 funcionalidade --> */}
-      <div class="paralax-montain mrgt-media">
-        <h1 class=" tieb  titulos t-home d-flex justify-content-center">Video</h1>
+      <div className="paralax-montain mrgt-media">
+        <h1 className=" tieb  titulos t-home d-flex justify-content-center">Video</h1>
       </div>
 
       {/* <!-- inicio video --> */}
@@ -142,7 +149,7 @@ return(
         <iframe  width="560" height="315"
           src="https://www.youtube.com/embed/4eoM26ZmHd0" title="YouTube video player" frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen></iframe> {/*style="border-radius: 20px 5px 20px 5px;"*/}
+          allowfullscreen style={{borderRadius: '20px 5px 20px 5px'}}></iframe> 
       </div>
       {/* <!-- fim video  --> */}
       <div class="paralax-montain mrgt-media">
@@ -155,7 +162,7 @@ return(
       </div>
       <div class="d-flex  mrgt-grande row m-0">
         <div class=" mrgl-grande col-12 col-sm-12 col-md-8 col-lg-8 " id="home-book">
-          <img src="./img/teste-ebook.jpg" alt="Imagem e-book" title="Imagem e-book" ></img> {/*style="width: 100%;"*/}
+          <img src="./img/teste-ebook.jpg" alt="Imagem e-book" title="Imagem e-book" style={{width: '100%'}}></img> 
         </div>
         <div class="col-12 col-sm-12 col-md-8 col-lg-8">
           <p class="titulos mrgx-media" id="home-tbook">Titulo do E-book</p>
@@ -187,7 +194,7 @@ return(
       {/* <!-- inicio parte institucional --> */}
       <div class="cards-insti mrgx-grande">
         <div class="insti-textos">
-          <p class="textos mrgt-pequena" >Somos um grupo de desenvolvedores que entendemos {/*style="clear: left;"*/}
+          <p class="textos mrgt-pequena" style={{clear: 'left'}} >Somos um grupo de desenvolvedores que entendemos 
             que a
             ansiedade no Brasil é um problema real, sério e que aumentou muito durante a pandemia. Por isso
             criamos o
@@ -202,7 +209,7 @@ return(
 
 
         <div class="insti-textos">
-          <p class="textos mrgt-pequena" >Ajudar pessoas que possuem problemas com {/*style="clear: left;"*/}
+          <p class="textos mrgt-pequena" style={{clear: 'left'}} >Ajudar pessoas que possuem problemas com 
             ansiedade sugerindo
             diversos exercícios de acordo com a personalidade da pessoa, e permitindo que ela personalize suas
             soluções.
@@ -218,7 +225,7 @@ return(
 
       <div class="cards-insti mrgx-grande mrgb-media">
         <div class="insti-textos">
-          <p class="textos mrgt-pequena" >Pretendemos nos tornar o maior canal acessível {/*style="clear: left;"*/}
+          <p class="textos mrgt-pequena" style={{clear: 'left'}}>Pretendemos nos tornar o maior canal acessível 
             para ajuda
             contra ansiedade do Brasil, sendo bem-visto e recomendado por conselhos de psicologia de todo o
             país. Nosso
@@ -229,18 +236,18 @@ return(
 
 
         <div class="insti-textos">
-          <p class="textos mrgt-pequena" >{/*style="clear: left;"*/}<span >{/*style="font-weight: bold;"*/}Humanização:
+          <p class="textos mrgt-pequena" style={{clear: 'left'}} ><span style={{fontWeight: 'bold'}}>Humanização:
             </span>
             Cuidado centrado nas pessoas, respeitando todos os usuários.</p>
-          <p class="textos mrgt-pequena" >{/*style="clear: left;"*/}<span >{/*style="font-weight: bold;"*/}Compreensão:
+          <p class="textos mrgt-pequena" style={{clear: 'left'}}><span style={{fontWeight: 'bold'}}>Compreensão:
             </span>
             Compreendemos que a ansiedade é um problema sério e que deve ser tratada com muita atenção.</p>
-          <p class="textos mrgt-pequena" >{/*style="clear: left;"*/}<span >{/*style="font-weight: bold;"*/}Gratidão:
+          <p class="textos mrgt-pequena" style={{clear: 'left'}}><span style={{fontWeight: 'bold'}}>Gratidão:
             </span> Somos
             gratos pela possibilidade de auxílio às pessoas principalmente por termos passado por momentos tão
             difíceis.
           </p>
-          <p class="textos mrgt-pequena" >{/*style="clear: left;"*/}<span >{/*style="font-weight: bold;"*/}Confiabilidade:
+          <p class="textos mrgt-pequena" style={{clear: 'left'}}><span style={{fontWeight: 'bold'}}>Confiabilidade:
             </span>
             Estabelecer relacionamentos abertos, transparentes e apoiados na confiança mútua.</p>
         </div>
