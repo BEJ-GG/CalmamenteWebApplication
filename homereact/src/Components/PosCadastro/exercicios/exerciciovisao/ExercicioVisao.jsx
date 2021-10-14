752e76dce10f45a46364a412d0a472b054c3443a
Jhoctan21 <rm88206@fiap.com.br>
2021-10-14 12:52:56 -0300

    .

--- homereact/src/Components/PosCadastro/exercicios/exerciciovisao/ExercicioVisao.jsx
+++ homereact/src/Components/PosCadastro/exercicios/exerciciovisao/ExercicioVisao.jsx
@@ -1,53 +1,52 @@
 import React from 'react'
 
-export default function ExercicioVisao() {
+class ExercicioVisao extends React.Component {
 
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         x: 0,
-    //         y: 0
-    //     }
-    // }
-
-    // _onMouseMove = (e) => {
-    //     const width = this.refs.ball.clienteWidth;
-    //     const heigth = this.refs.ball.clienteHeigth;
-    //     const oX = (e.nativeEvent.oofsetX/width) * 100;
-    //     const oY = (e.nativeEvent.oofsetX/heigth) * 100;
-    //     // console.log(Math.floor(oX), Math.floor(oY));
-
-    //     this.setState({
-    //         x: oX,
-    //         y: oY
-    //     })
-    //     const {x, y} = this.state;
-    //     const maskStyle ={
-    //         '--maskX': x,
-    //         '--maskY' y
-
-    //     }
+    constructor(props) {
+        super(props);
+        this.state = {
+            x: 0,
+            y: 0
+        }
     }
-    return (
-        <>
-            <body className="fundoOl">
-                <h1 className="escr-olho">Observe o ambiente a sua volta.
-                    Liste mentalmente 5 coisas que você consegue enxergar.</h1
-                <div className="eyes">
-                    <div className="eye">
-                        <div className="ball" onMouseMove={this._onMouseMove} ref="ball"></div>
-                    </div>
-                    <div className="eye">
-                        <div className="ball" onMouseMove={this._onMouseMove} ref="ball"></div>
+
+    _onMouseMove = (e) => {
+        const width = this.refs.eyes.clientWidth;
+        const heigth = this.refs.eyes.clientHeigth;
+        const oX = (e.nativeEvent.oofsetX / width) * 100;
+        const oY = (e.nativeEvent.oofsetX / heigth) * 100;
+        // console.log(Math.floor(oX), Math.floor(oY));
+
+        this.setState({
+            x: oX,
+            y: oY
+        });
+
+    }
+    render() {
+        const { x, y } = this.state;
+        const maskStyle = {
+            '--maskX': x,
+            '--maskY': y
+        }
+
+        return (
+            <>
+                <div className="fundoOl">
+                    <h1 className="escr-olho">Observe o ambiente a sua volta.
+                        Liste mentalmente 5 coisas que você consegue enxergar.</h1>
+                    <div className="eyes" onMouseMove={this._onMouseMove} ref="eyes" style={maskStyle}>
+                        <div className="eye">
+                            <div className="ball"></div>
+                        </div>
+                        <div className="eye" >
+                        <div className="ball" ></div>
                     </div>
                 </div>
-                {/* <script>
-                </script>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
-                    integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
-                </script> */}
-            </body>
+            </div>
 
-        </>
-    )
+            </>
+        )
+    }
 }
+export default ExercicioVisao;
