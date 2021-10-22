29887aaecef164233f40ff99a7bb34f42293a04e
Jhoctan21 <rm88206@fiap.com.br>
2021-10-22 13:25:01 -0300

    Iframe completo rodando

--- homereact/src/Components/PosCadastro/exercicios/exerciciomao/Exerciciomao.jsx
+++ homereact/src/Components/PosCadastro/exercicios/exerciciomao/Exerciciomao.jsx
@@ -5,12 +5,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Exerciciomao() {
     return (
         <>
-            <body className="fundo-mao">
+            <div className="fundo-mao">
                     <h1 className="escr-olho">Liste mentalmente 4 coisas que você consegue tocar.</h1>
 
                     <div class="col-12 row in-ce">
                         <div className="col-6">
-                            <FontAwesomeIcon icon={faPencilAlt} className="laex" />
+                            <FontAwesomeIcon icon={faPencilAlt} className="laex" style={{marginBottom: '68%'}}/>
                         </div>
 
                         <div className="col-6">
@@ -27,13 +27,7 @@ export default function Exerciciomao() {
                             <FontAwesomeIcon icon={faCouch} className="soex" />
                         </div>
                     </div>
-                    
-
-                    {/* <script>
-                    </script>
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
-                    </script> */}
-            </body>
+            </div>
 
         </>
     )
--- homereact/src/Components/PosCadastro/exercicios/exerciciovisao/ExercicioVisao.jsx
+++ homereact/src/Components/PosCadastro/exercicios/exerciciovisao/ExercicioVisao.jsx
@@ -1,36 +1,37 @@
 import React from 'react'
 
-class ExercicioVisao extends React.Component {
+// class ExercicioVisao extends React.Component 
+export default function ExercicioVisao() {
 
     
 
 
-    _onMouseMove = (e) => {
-        const width = this.refs.eyes.clientWidth;
-        const heigth = this.refs.eyes.clientHeigth;
-        const oX = (e.nativeEvent.oofsetX / width) * 100;
-        const oY = (e.nativeEvent.oofsetX / heigth) * 100;
-        // console.log(Math.floor(oX), Math.floor(oY));
+    // _onMouseMove = (e) => {
+    //     const width = this.refs.eyes.clientWidth;
+    //     const heigth = this.refs.eyes.clientHeigth;
+    //     const oX = (e.nativeEvent.oofsetX / width) * 100;
+    //     const oY = (e.nativeEvent.oofsetX / heigth) * 100;
+    //     // console.log(Math.floor(oX), Math.floor(oY));
 
-        this.setState({
-            x: oX,
-            y: oY
-        });
+    //     this.setState({
+    //         x: oX,
+    //         y: oY
+    //     });
 
-    }
-    render() {
-        const { x, y } = this.state;
-        const maskStyle = {
-            '--maskX': x,
-            '--maskY': y
-        }
+    // }
+    // render() {
+    //     const { x, y } = this.state;
+    //     const maskStyle = {
+    //         '--maskX': x,
+    //         '--maskY': y
+    //     }
 
         return (
             <>
                 <div className="fundoOl">
                     <h1 className="escr-olho">Observe o ambiente a sua volta.
                         Liste mentalmente 5 coisas que você consegue enxergar.</h1>
-                    <div className="eyes" onMouseMove={this._onMouseMove} ref="eyes" style={maskStyle}>
+                    <div className="eyes" /*onMouseMove={this._onMouseMove} ref="eyes" /*style={maskStyle}*/>
                         <div className="eye">
                             <div className="ball"></div>
                         </div>
@@ -42,7 +43,7 @@ class ExercicioVisao extends React.Component {
 
             </>
         )
-    }
+    // }
 
 }
-export default ExercicioVisao;
+// export default ExercicioVisao;
